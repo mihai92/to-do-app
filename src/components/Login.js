2dc0087d36608cb79391b9d9b88c4fcf4ba00ee7
silviuhogas <silviu.hogas@yahoo.com>
2023-10-18 11:59:24 +0300

    Rute pentru login,register, dashboard

---
+++ src/components/Login.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // logica buton login
+    console.log('Login clicked');
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form>
+        <label>Email:</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+        <label>Password:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <button type="button" onClick={handleLogin}>
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
