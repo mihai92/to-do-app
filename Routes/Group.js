1e3be960a04244d2e58ca74391b4c9ccdfdfbf98
maxedo <maxitiansergiu@gmail.com>
2023-12-23 18:46:52 +0200

    new middleware and route modiffications

--- Routes/Group.js
+++ Routes/Group.js
@@ -9,7 +9,6 @@ router.post("/group",Autentificare,async (req,res)=>{
     const {Nume}=req.body;
     try{
         const [query]=await db.execute("INSERT INTO GroupT(Id_Admin,Nume) VALUES(?,?)",[req.auth.id,Nume]);
-        db.end();
         res.status(200).json({message:"Cererea a trecut cu succes"});
     }catch(err){
         res.status(500);
@@ -19,7 +18,6 @@ router.post("/group",Autentificare,async (req,res)=>{
 router.get("/group",Autentificare,async (req,res)=>{
     try{
     const [query]=await db.execute("SELECT * FROM GroupT as g,Members as m WHERE g.Id_Group=m.Id_Group AND Id_Membru=?",[req.auth.id]);
-    db.end();
     res.status(200).json(query);
     }catch(err){
         res.status(500).json(err);
--- Routes/Notifications.js
+++ Routes/Notifications.js
@@ -8,18 +8,17 @@ const { Autentificare } = require("../Middleware/Auth");
 router.post("/invite/:InvitedId/:GroupId",Autentificare,async (req,res)=>{
     try{
         const [queryUser]=await db.execute("SELECT Nume FROM Users WHERE Id=?",[req.auth.id]);
-        const [queryGroup]=await db.execute("SELECT Group FROM Users WHERE Id=?",[req.params.GroupId]);
+        const [queryGroup]=await db.execute("SELECT Nume FROM GroupT WHERE Id=?",[req.params.GroupId]);
         const nume=queryUser[0];
         const numegrup=queryGroup[0];
-        const [query]=await db.execute("INSERT INTO Notifications(Id_Emitator,Id_Group,Id_Interceptor ,Mesaj, Acceptat, Vizibilitate_Acceptare) VALUES(?,?,?,Ai fost invitat de catre ? pentru a participa in grupul ?,false,true)",[req.auth.id,req.params.GroupId,req.params.InvitedId,nume,numegrup]);
-        db.end();
+        const [query]=await db.execute("INSERT INTO Notifications(Id_Emitator,Id_Group,Id_Interceptor ,Mesaj, Acceptat, Vizibilitate_Acceptare) VALUES(?,?,?,Ai fost invitat de catre ? pentru a participa in grupul ?,false,true)",[req.auth.id,req.params.GroupId,req.params.InvitedId,nume.Nume,numegrup.Nume]);
     }catch(err){
         res.status(500).json(err);
     }
 });
 
 
-router.put('/invite/:InviteId',Autentificare, async(req,res)=>{
+router.put('/inviteResponse/:InviteId',Autentificare, async(req,res)=>{
     const {bool}=req.body;
     try{
         if(bool){
@@ -28,7 +27,6 @@ router.put('/invite/:InviteId',Autentificare, async(req,res)=>{
         else{
         const [query2]=await db.execute("UPDATE Notifications SET Acceptat=false AND Vizibilitate_Acceptare=false  WHERE Id=?",[req.params.InviteId])
         }
-        db.end();
     }catch(err){
         res.status(500).json(err);
     }
