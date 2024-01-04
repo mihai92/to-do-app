2f98a96576598baeb2022319e34be40343943552
maxedo <maxitiansergiu@gmail.com>
2024-01-04 22:37:02 +0200

    route changes and issues solved

--- Middleware/Auth.js
+++ Middleware/Auth.js
@@ -5,23 +5,21 @@ require('dotenv').config()
 async function Autentificare(req,res,next){
     const Headercerere=req.headers.authorization;
     const jeton=Headercerere.split('Bearer ')[1];
+    try{
     if(jeton==null){
         return res.status(401).json({message:"Acces neautorizat"})
     }
     jwt.verify(jeton,process.env.ACCESS_TOKEN_SECRET,async (err,UserId)=>{
-        try{
+       
             if(err){
                 return res.status(403).send(err);
             }else{
                 req.auth=UserId;
                 next();  
-            }
-        }catch(e){
-            res.status(422).send("Jeton invalid")
-        }
-        
-    
-
-    })
-}
+            }          
+        })
+    }catch(e){
+        res.status(422).send("Jeton invalid")
+    }
+    }
 module.exports = {Autentificare};
--- Routes/Group_Activity.js
+++ Routes/Group_Activity.js
@@ -6,11 +6,14 @@ const { Autentificare } = require("../Middleware/Auth");
 const {VerificareRol}=require("../Middleware/Role");
 
 
+
+// Ruta folosita pentru atribuirea unui task cuiva de catre admin
 router.post("/GActivity/:Id_Group/:Id_Membru",Autentificare,VerificareRol, async (req,res)=>{
         try{
             const {Nume,Deadline}=req.body;
-            const [query]=await db.execute("INSERT INTO Group_Activity(Id_Group, Id_Membru, Nume, Status, Deadline) Values(?,?,?, false, ?)",[req.params.Id_Group,req.params.Id_Membru,Nume,Deadline]);
-            const string=""
+            const [query]=await db.execute("INSERT INTO Group_Activity(Id_Group, Id_Membru, Nume, Status,Acceptat, Deadline) Values(?,?,?,false, false, ?)",[req.params.Id_Group,req.params.Id_Membru,Nume,Deadline]);
+            const [numegrup]=await db.execute("SELECT Nume from Groupt WHERE Id_Group=?",[req.params.Id_Group]);
+            const string="Ai primit un task nou in Grupul "+numegrup[0].Nume;
             const [query2]=await db.execute("INSERT INTO Notifications(Id_Group, Id_Emitator, Id_Interceptor, Mesaj, Acceptat, Vizibilitate_Acceptat) VALUES(?,?,?,?, false, true)",[req.params.Id_Group, req.auth.id, req.params.Id_Membru,string])
             res.send(200).json({Message:"Activitatea a fost atribuita!"});
         }catch(err){
@@ -18,17 +21,52 @@ router.post("/GActivity/:Id_Group/:Id_Membru",Autentificare,VerificareRol, async
         }
 })
 
-router.put("/GActivity/:Id_Activity",Autentificare, async(req,res)=>{
+
+// Ruta folosita pentru a vedea task-urile dintr-un grup
+router.get("/GActivity/:Id_Group",Autentificare, async (req,res)=>{
+    try{
+        const [verificare]=await db.execute("SELECT * FROM Members WHERE Id_Group=? AND Id_Membru=?",[req.params.Id_Group,req.auth.id]);
+        
+        if(verificare[0]===null){
+            res.send(403).json({Message:"Utilizatorul nu face parte din grupul ales"});
+        }
+        const [query]=await db.execute("SELECT * FROM Group_Activity WHERE Id_Group=? AND Acceptat=true",[req.params.Id_Group]);
+        
+        res.send(200).json(query);
+    }catch(err){
+        res.send(500).json(err);
+    }
+})
+
+
+
+
+
+// Ruta folosita pentru a accepta sau respinge un task atribuit tie ca membru al grupului
+router.put("/GActivity/:Id_Group/:Id_Activity",Autentificare, async(req,res)=>{
     try{
         const {acceptare}=req.body;
+        const [verificare]=await db.execute("SELECT * FROM Members WHERE Id_Group=? AND Id_Membru=?",[req.params.Id_Group,req.auth.id]);
+        
+        if(verificare[0]===null){
+            res.send(403).json({Message:"Utilizatorul nu face parte din grupul ales"});
+        }
+
+        if(acceptare===false){
+            const [query]=await db.execute("DELETE FROM Group_Activity WHERE Id_Activitate=?",[req.params.Id_Activity])
+            res.send(200).json({Message:"Activitatea a fost respinsa"});
+        }
 
 
+        const [query2]=await db.execute("UPDATE Group_Activity WHERE Id_Activitate=? SET Acceptat=true",[req.params.Id_Activity]);
         res.send(200).json({Message:"Activitatea a fost acceptata"});
     }catch(err){
         res.send(500).json(err);
     }
 })
 
+
+// Ruta folosita pentru a sterge un task intr-un grup ca administrator
 router.delete("/GActiviity/:Id_Activity",Autentificare,VerificareRol, async(req,res)=>{
     try{
         const [query]=await db.execute("DELETE FROM Group_Activity WHERE Id_Activitate=?",[req.params.Id_Activity]);
